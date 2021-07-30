fa88812d9714b718a2bdba56a7ba8ca3cf2b5fe8
Shahryar Hejazipour <shahryar.hejazipour@outlook.com>
2021-07-30 18:53:25 +0430

    setting up navigation between bottom navigation's items

--- app/src/main/java/com/tispunshahryar960103/onlineshop/view/activity/MainActivity.kt
+++ app/src/main/java/com/tispunshahryar960103/onlineshop/view/activity/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.tispunshahryar960103.onlineshop.view.activity
 
+
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.tispunshahryar960103.onlineshop.R
+import android.view.MenuItem
 
-class MainActivity : AppCompatActivity() {
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationBarView
+
+import com.tispunshahryar960103.onlineshop.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+
+    lateinit var navController: NavController
+    lateinit var appBar: AppBarConfiguration
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        appBar = AppBarConfiguration.Builder(R.navigation.navigation).build()
+
+        navController = Navigation.findNavController(this, R.id.fragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBar)
+
+        NavigationUI.setupWithNavController(bottom_Navigation, navController)
+
+        bottom_Navigation.setOnItemSelectedListener(this)
+
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+            R.id.menu_shop_home -> navController.navigate(R.id.postFragment)
+            R.id.menu_profile -> navController.navigate(R.id.profileFragment)
+            R.id.menu_cart -> navController.navigate(R.id.cartFragment)
+
+
+        }
+
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp()
+    }
+
 }
+
+
--- app/src/main/java/com/tispunshahryar960103/onlineshop/view/fragment/Test.kt
+++
@@ -1,4 +0,0 @@
-package com.tispunshahryar960103.onlineshop.view.fragment
-
-class Test {
-}
